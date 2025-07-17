05ffae15bce812bb9229b50c654a23960392b991
Alex Shyba <alex.shyba@uniform.dev>
2025-07-17 15:08:19 -0700

    fix: sitemap with dynamic pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
 import type { MetadataRoute } from 'next';
 import { ProjectMapClient, getNodeActiveCompositionEdition } from '@uniformdev/project-map';
 import localesConfig from '@/i18n/locales.json';
+import { ContentClient } from '@uniformdev/canvas';
 
 const projectMap = new ProjectMapClient({
   apiHost: process.env.UNIFORM_CLI_BASE_URL! || 'https://uniform.app',
@@ -18,12 +19,16 @@ const BASE_URL = process.env.BASE_URL ? `https://${process.env.BASE_URL}` : '';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const domain = BASE_URL || 'http://localhost:3000';
   const { nodes } = await projectMap.getNodes({ withCompositionData: true });
+  const contentClient = new ContentClient({
+    apiKey: process.env.UNIFORM_API_KEY!,
+    projectId: process.env.UNIFORM_PROJECT_ID!,
+  });
 
   if (!nodes) return [];
 
   const isLocalized = localesConfig?.locales?.length > 0;
 
-  return nodes.flatMap(node => {
+  const nodePromises = nodes.map(async node => {
     if (!isLocalized || !node.path?.includes(':locale')) {
       const edition = getNodeActiveCompositionEdition({
         node,
@@ -34,24 +39,63 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         {
           url: `${domain}${node.path}`,
           lastModified: edition?.modified,
-          changeFrequency: 'daily',
+          changeFrequency: 'daily' as const,
           priority: 1,
         },
       ];
     }
 
-    return localesConfig.locales.map(locale => {
+    const localePromises = localesConfig.locales.map(async locale => {
       const edition = getNodeActiveCompositionEdition({
         node,
         targetLocale: locale,
       });
 
-      return {
-        url: `${domain}${node.path?.replace(':locale', locale)}`,
-        lastModified: edition?.modified,
-        changeFrequency: 'daily',
-        priority: 1,
-      };
+      if (!node.path) return [];
+
+      const url = `${domain}${node.path.replace(':locale', locale)}`;
+
+      const isDynamicNode = node?.pathSegment?.startsWith(':');
+      if (isDynamicNode && node.pathSegment) {
+        let slugs: string[] = [];
+        if (node.pathSegment === (':article-slug')) {
+          const { entries } = await contentClient.getEntries({
+            filters: {
+              'type[eq]': 'article', // Replace with your content type
+            },
+          });
+
+          slugs = entries.map((e) => e.entry._slug).filter((slug): slug is string => Boolean(slug));
+        } else if (node.pathSegment === (':product-slug')) {
+          const { entries } = await contentClient.getEntries({
+            filters: {
+              'type[eq]': 'product', // Replace with your content type
+            },
+          });
+          slugs = entries.map((e) => e.entry._slug).filter((slug): slug is string => Boolean(slug));
+        }
+        return slugs.map(s => {
+          return {
+            url: url.replace(node.pathSegment!, s),
+            lastModified: edition?.modified,
+            changeFrequency: 'daily' as const,
+            priority: 1,
+          };
+        });
+      } else {
+        return [{
+          url,
+          lastModified: edition?.modified,
+          changeFrequency: 'daily' as const,
+          priority: 1,
+        }];
+      }
     });
+
+    const resolvedLocales = await Promise.all(localePromises);
+    return resolvedLocales.flat();
   });
+
+  const resolvedNodes = await Promise.all(nodePromises);
+  return resolvedNodes.flat();
 }
